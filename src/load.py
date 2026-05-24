5809d8031f2727c51856ba97481c50e80b448346
Egor Ermolaev <egorermolaev635@gmail.com>
2026-05-24 23:51:10 +0400

    Update load.py

--- src/load.py
+++ src/load.py
@@ -1,48 +1,44 @@
+import argparse
 from pathlib import Path
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import os
 
 MART_DIR = Path(os.getenv("MART_DIR", "data/mart/variant_03"))
 TABLE_NAME = os.getenv("TABLE_NAME", "mart_weather")
-
 DB_URL = os.getenv(
     "DB_URL",
     "postgresql+psycopg2://analytics:analytics_pass@localhost:5432/analytics_db"
 )
 
-def get_latest_mart_file(mart_dir: Path) -> Path:
-    files = list(mart_dir.glob("*.csv"))
-
-    if not files:
-        raise FileNotFoundError(f"В папке нет mart-файлов: {mart_dir}")
-
-    latest_file = max(files, key=lambda f: f.stat().st_mtime)
-    return latest_file
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--date", required=True)
+    args = parser.parse_args()
+    target_date = args.date
+
     if not MART_DIR.exists():
         raise FileNotFoundError(f"Folder with mart not found: {MART_DIR}")
-    mart_path = get_latest_mart_file(MART_DIR)
-    df = pd.read_csv(mart_path)
-    print("Loaded mart file:", mart_path)
-    print("Shape", df.shape)
-    print("Columns:", list(df.columns))
-    print("Dtypes:")
-    print(df.dtypes)
+        
+    mart_path = MART_DIR / f"mart_daily_{target_date}.csv"
+    if not mart_path.exists():
+        raise FileNotFoundError(f"Missing file: {mart_path}")
 
+    df = pd.read_csv(mart_path)
     engine = create_engine(DB_URL)
 
     with engine.begin() as conn:
+        delete_query = text(f"DELETE FROM {TABLE_NAME} WHERE date = :target_date")
+        conn.execute(delete_query, {"target_date": target_date})
+
         df.to_sql(
             TABLE_NAME,
-            con = conn,
-            if_exists="replace",
+            con=conn,
+            if_exists="append",
             index=False
         )
-    print(f"loaded rows to postgres: {len(df)}")
-    print(f"table: {TABLE_NAME}")
-    print(f"Table '{TABLE_NAME}' loaded successfully.")
+
 
 if __name__ == "__main__":
     main()
