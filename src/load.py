4ec3a02d193a2c8a9b03b995b477b8ae8adeb68b
Egor Ermolaev <egorermolaev635@gmail.com>
2026-05-31 23:28:28 +0400

    Update load.py

--- src/load.py
+++ src/load.py
@@ -1,14 +1,17 @@
 import argparse
-from pathlib import Path
-import pandas as pd
-from sqlalchemy import create_engine, text
 import os
+from pathlib import Path
 
-MART_DIR = Path(os.getenv("MART_DIR", "data/mart/variant_03"))
+import pandas as pd
+from sqlalchemy import create_engine, inspect, text
+
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+MART_DIR = Path(os.getenv("MART_DIR", PROJECT_ROOT / "data" / "mart" / "variant_03"))
 TABLE_NAME = os.getenv("TABLE_NAME", "mart_weather")
 DB_URL = os.getenv(
     "DB_URL",
-    "postgresql+psycopg2://analytics:analytics_pass@localhost:5432/analytics_db"
+    "postgresql+psycopg2://analytics:analytics_pass@localhost:5432/analytics_db",
 )
 
 
@@ -20,7 +23,7 @@ def main():
 
     if not MART_DIR.exists():
         raise FileNotFoundError(f"Folder with mart not found: {MART_DIR}")
-        
+
     mart_path = MART_DIR / f"mart_daily_{target_date}.csv"
     if not mart_path.exists():
         raise FileNotFoundError(f"Missing file: {mart_path}")
@@ -29,15 +32,22 @@ def main():
     engine = create_engine(DB_URL)
 
     with engine.begin() as conn:
-        delete_query = text(f"DELETE FROM {TABLE_NAME} WHERE date = :target_date")
-        conn.execute(delete_query, {"target_date": target_date})
+        table_exists = inspect(conn).has_table(TABLE_NAME)
+
+        if table_exists:
+            delete_query = text(f"DELETE FROM {TABLE_NAME} WHERE date = :target_date")
+            conn.execute(delete_query, {"target_date": target_date})
 
         df.to_sql(
             TABLE_NAME,
             con=conn,
             if_exists="append",
-            index=False
+            index=False,
         )
+
+    print(f"loaded rows to postgres: {len(df)}")
+    print(f"table: {TABLE_NAME}")
+    print(f"source: {mart_path}")
 
 
 if __name__ == "__main__":
